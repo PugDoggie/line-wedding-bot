cf403def541d1fc92fc143b90ebacd9a80639843
DavidLin <david88897546@gmail.com>
2025-09-18 17:34:12 +0800

    Update contents

--- src/main/java/com/wedding/invite/repository/BlessingRepository.java
+++ src/main/java/com/wedding/invite/repository/BlessingRepository.java
@@ -1,8 +1,8 @@
 package com.wedding.invite.repository;
 
+import com.wedding.invite.model.Blessing;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.wedding.invite.model.Blessing;
 
 @Repository
 public interface BlessingRepository extends JpaRepository<Blessing, Long> {
