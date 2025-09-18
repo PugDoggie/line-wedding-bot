a9e03a4132d820448ca8740bad4acb9545c78da0
DavidLin <david88897546@gmail.com>
2025-09-18 17:42:40 +0800

    Update contents

--- src/main/java/com/wedding/invite/LineWeddingBotApplication.java
+++ src/main/java/com/wedding/invite/LineWeddingBotApplication.java
@@ -2,8 +2,12 @@ package com.wedding.invite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.wedding.invite.repository")
+@EntityScan(basePackages = "com.wedding.invite.model")
 public class LineWeddingBotApplication {
     public static void main(String[] args) {
         SpringApplication.run(LineWeddingBotApplication.class, args);
